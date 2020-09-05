22ca6d426a7ddf2d4f3eb366304eea31ffcc9fa4
Raynnon <assante.f@gmail.com>
2020-09-05 23:55:25 +0200

    finished the calculator

--- calculator.js
+++ calculator.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/index.html");
+  });
+
+  app.post("/", function(req, res) {
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+    let result= num1 + num2;
+
+    res.send("Result: " + result);
   });
   
   app.listen(3000, function() {
