e2ffd40e0808dacd23afdc47a3db7e449d240053
Raynnon <assante.f@gmail.com>
2020-09-06 19:34:36 +0000

    added a BMI calculator

--- calculator.js
+++ calculator.js
@@ -8,12 +8,25 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + "/index.html");
   });
 
-  app.post("/", function(req, res) {
-    let num1 = Number(req.body.num1);
-    let num2 = Number(req.body.num2);
-    let result= num1 + num2;
+  app.post('/', function(req, res) {
+    const num1 = Number(req.body.num1);
+    const num2 = Number(req.body.num2);
+    const result = num1 + num2;
 
     res.send("Result: " + result);
+  });
+
+  app.get('/bmiCalculator', function(req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+  });
+
+  app.post('/bmiCalculator', function(req, res) {
+    const height = Number(req.body.height);
+    const weight = Number(req.body.weight);
+
+    const bmi = weight / (height * height);
+
+    res.send("Your BMI is " + bmi);
   });
   
   app.listen(3000, function() {
